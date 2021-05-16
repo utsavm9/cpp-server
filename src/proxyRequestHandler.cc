c8eceabe6cf7e4de9af24ff2bb2a8d75095545e0
stevejiang <stevejjiang@g.ucla.edu>
2021-05-16 23:37:25 +0000

    Updated implementation of proxy request handler on redirect as per clarification on piazza @237
    This includes getting rid of automatic redirect handling, removing
    redirect depth from config, and making proper modifications to
    the proxied redirect location header.
    
    Change-Id: I8215873526321e4fba9702c0419d696c7c588038

--- src/proxyRequestHandler.cc
+++ src/proxyRequestHandler.cc
@@ -17,7 +17,6 @@ ProxyRequestHandler::ProxyRequestHandler(const std::string &p, const NginxConfig
 	if (url_prefix.size() > 0 && url_prefix[url_prefix.size() - 1] == '/') {
 		url_prefix.erase(url_prefix.size() - 1);
 	}
-	max_redirects = 2;
 	try {
 		for (size_t i = 0; i < config.statements_.size(); i++) {
 			std::shared_ptr<NginxConfigStatement> st = config.statements_.at(i);
@@ -27,16 +26,12 @@ ProxyRequestHandler::ProxyRequestHandler(const std::string &p, const NginxConfig
 			if (!strcmp(st->tokens_.at(0).c_str(), "port")) {
 				m_port = st->tokens_.at(1);
 			}
-			if (!strcmp(st->tokens_.at(0).c_str(), "max_redirects")) {
-				max_redirects = std::stoi(st->tokens_.at(1));
-			}
 		}
 		if (proxy_dest.empty() || m_port.empty()) {
 			throw std::runtime_error("Missing required config field");
 		}
 		INFO << "ProxyRequestHandler for " << url_prefix << " -> Proxy destination: " << proxy_dest;
 		INFO << "ProxyRequestHandler for " << url_prefix << " -> Port: " << m_port;
-		INFO << "ProxyRequestHandler for " << url_prefix << " -> Max Redirects: " << max_redirects;
 		invalid_config = false;
 	} catch (std::exception &e) {
 		FATAL << "exception occurred : " << e.what();
@@ -50,7 +45,17 @@ http::response<http::string_body> ProxyRequestHandler::req_synchronous(std::stri
 	beast::tcp_stream stream(ioc);
 	tcp::resolver::query query(host, port);
 	auto const results = resolver.resolve(query);
+	INFO << "ProxyRequestHandler attempting to connect to host "
+	     << host
+	     << " on port "
+	     << port;
 	stream.connect(results);
+	INFO << "ProxyRequestHandler successfully connected to host "
+	     << host
+	     << " on "
+	     << stream.socket().remote_endpoint().address().to_string()
+	     << ":"
+	     << stream.socket().remote_endpoint().port();
 	http::write(stream, request);
 	beast::flat_buffer buffer;
 	http::response<http::string_body> res;
@@ -82,47 +87,21 @@ http::response<http::string_body> ProxyRequestHandler::handle_request(const http
 		proxy_req.prepare_payload();
 		http::response<http::string_body> res = req_synchronous(proxy_dest, m_port, proxy_req);
 
-		std::size_t redirect_count = 0;
-		// Automatically resolve http redirects
-		while (res.result_int() == 301 || res.result_int() == 302) {
-			if (redirect_count >= max_redirects) {
-				throw std::runtime_error("Too many redirects");
-			}
-			std::string host;
-			std::string target = "/";
-			std::string location = res.at(http::field::location).to_string();
-
-			// Server currently cannot handle https as per piazza @225
-			if (location.find("https://") != std::string::npos) {
-				throw std::runtime_error("HTTPS redirects unsupported");
-			}
-
-			// Following location formats supported:
-			// 1. http://host.com/path -> new host, new target
-			// 2. host.com/path -> new host, new target
-			// 3. host.com -> new host, old target
-			// 4. /path -> old host, new target
-			std::size_t scheme_end = location.find("//");
-			if (scheme_end != std::string::npos) {
-				location = location.substr(scheme_end + 2);
-			}
-			std::size_t path_start = location.find("/");
-			if (path_start != std::string::npos) {
-				target = location.substr(path_start);
-				host = location.substr(0, path_start);
-			} else {
-				host = location;
-			}
-			if (host.length() > 0) {
-				proxy_req.set(http::field::host, host);
-			}
-			proxy_req.target(target);
-			res = req_synchronous(host, m_port, proxy_req);
-			redirect_count++;
+		if (http::to_status_class(res.result()) != http::status_class::redirection ||
+		    res.find(http::field::location) == res.end()) {
+			return res;
 		}
+
+		// Handle redirect
+		std::string redirect_location = res.at(http::field::location).to_string();
+		if (redirect_location.length() > 0 && redirect_location[0] == '/') {
+			// Relative url redirects should be made relative to the proxy
+			redirect_location = proxy_dest + redirect_location;
+		}
+		res.set(http::field::location, redirect_location);
 		return res;
 	} catch (std::exception &e) {
-		ERROR << "Exception occurred in ProxyRequestHanlder for " << url_prefix << ": " << e.what();
+		ERROR << "Exception occurred in ProxyRequestHandler for " << url_prefix << ": " << e.what();
 		return RequestHandler::internal_server_error();
 	}
 }
